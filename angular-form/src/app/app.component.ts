90d3c0c30c984cf76be3b71a74c52c2e328976b5
StefanAmur <stefan.amur22@gmail.com>
2021-11-29 19:11:18 +0100

    added form and set invalid class

---
+++ angular-form/src/app/app.component.ts
@@ -0,0 +1,25 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'angular-form';
+// }
+
+import { Component } from '@angular/core';
+import { Friend } from './friend';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-form';
+  languages: any = ['HTML', 'CSS', 'JS', 'PHP'];
+
+  public friend = new Friend('', '', '', '', '');
+}
---
+++ angular-form/src/app/friend.ts
@@ -0,0 +1,21 @@
+export class Friend {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
+  language?: string;
+
+  constructor(
+    firstName?: string,
+    lastName?: string,
+    email?: string,
+    phone?: string,
+    language?: string
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.language = language;
+  }
+}
